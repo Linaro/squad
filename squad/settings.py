2095d1e302d11d40bc949e2d7aaa45f179bf6e8f
Antonio Terceiro <terceiro@softwarelivre.org>
2016-05-24 18:08:08 -0300

    Add three skeleton apps: core, api, and frontend

--- squad/settings.py
+++ squad/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'squad.core',
+    'squad.api',
+    'squad.frontend',
 ]
 
 MIDDLEWARE_CLASSES = [
