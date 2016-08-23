b815c8d42260e0189fbc54d74c5867c742f2aeaf
Antonio Terceiro <antonio.terceiro@linaro.org>
2016-08-23 14:43:55 -0300

    frontend: optionally require authentication

--- squad/frontend/views.py
+++ squad/frontend/views.py
@@ -1,9 +1,26 @@
+import os
+
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
 from squad.core.models import Project
 
 
+PUBLIC_SITE = bool(os.getenv('SQUAD_PUBLIC_SITE'))
+
+
+def login_required_on_private_site(func):
+    if PUBLIC_SITE:
+        return func
+    else:
+        @login_required
+        def wrapper(request, *args, **kwargs):
+            return func(request, *args, **kwargs)
+        return wrapper
+
+
+@login_required_on_private_site
 def home(request):
     context = {
         'projects': Project.objects.all(),
@@ -11,9 +28,11 @@ def home(request):
     return render(request, 'squad/index.html', context)
 
 
+@login_required_on_private_site
 def group(request, group_slug):
     pass
 
 
+@login_required_on_private_site
 def project(request, group_slug, project_slug):
     pass
