671e825564f714751ddad36413b5af0984cce370
Antonio Terceiro <antonio.terceiro@linaro.org>
2016-08-29 15:57:03 -0300

    test_code_quality: run flake8 on the entire tree

--- test/test_code_quality.py
+++ test/test_code_quality.py
@@ -7,4 +7,4 @@ from django.test import TestCase
 class TestCodeQuality(TestCase):
 
     def test_flake8(self):
-        subprocess.check_call(['flake8', 'squad/'])
+        subprocess.check_call(['flake8'])
