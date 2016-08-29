0f02be75bf4f9c021f3fa37c728bae39b267bc6b
Antonio Terceiro <antonio.terceiro@linaro.org>
2016-08-29 17:53:43 -0300

    test_code_quality: skip flake8 is not installed

--- test/test_code_quality.py
+++ test/test_code_quality.py
@@ -1,10 +1,12 @@
 import subprocess
+import shutil
 
 
 from django.test import TestCase
 
-
-class TestCodeQuality(TestCase):
-
-    def test_flake8(self):
-        subprocess.check_call(['flake8'])
+if shutil.which('flake8'):
+    class TestCodeQuality(TestCase):
+        def test_flake8(self):
+            subprocess.check_call(['flake8'])
+else:
+    print("I: skipping flake8 test (flake8 not available)")
