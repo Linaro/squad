e08c792c241153da730bf7cac226c3df25fcbbfe
Antonio Terceiro <antonio.terceiro@linaro.org>
2016-08-29 15:46:43 -0300

    move flake8 parameters to setup.cfg

--- test/test_code_quality.py
+++ test/test_code_quality.py
@@ -7,6 +7,4 @@ from django.test import TestCase
 class TestCodeQuality(TestCase):
 
     def test_flake8(self):
-        # only ignore the long lines rule; sometimes it is very difficult to
-        # keep it under 80 columns.
-        subprocess.check_call(['flake8', '--ignore=E501,F401', 'squad/'])
+        subprocess.check_call(['flake8', 'squad/'])
