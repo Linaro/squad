63280478b74c3c83f82d13dcdb268879ab399c76
Antonio Terceiro <antonio.terceiro@linaro.org>
2016-08-29 17:39:05 -0300

    parse metrics data

--- squad/core/data.py
+++ squad/core/data.py
@@ -1,4 +1,5 @@
 import json
+from statistics import mean
 
 
 class JSONTestDataParser(object):
@@ -11,18 +12,52 @@ class JSONTestDataParser(object):
         input_data = json.loads(test_data)
         data = []
         for key, value in input_data.items():
-            group_name = None
-            test_name = key
-
-            parts = key.split('/')
-            # TODO: more than 2 parts (i.e. more than one /) is not valid
-            if len(parts) == 2:
-                group_name = parts[0]
-                test_name = parts[1]
-
+            group_name, test_name = parse_name(key)
             data.append({
                 "group_name": group_name,
                 "test_name": test_name,
                 "pass": value == 'pass',
             })
+        return data
+
+
+def parse_name(input_name):
+    group_name = None
+    name = input_name
+
+    parts = input_name.split('/')
+    if len(parts) > 1:
+        group_name = '/'.join(parts[0:-1])
+        name = parts[-1]
+
+    return (group_name, name)
+
+
+def parse_metric(value):
+    if isinstance(value, list):
+        return mean(value), value
+    else:
+        return value, [value]
+
+
+class JSONMetricDataParser(object):
+    """
+    Parser for JSON metric data
+    """
+
+    @staticmethod
+    def __call__(json_text):
+        input_data = json.loads(json_text)
+        data = []
+
+        for key, value in input_data.items():
+            group_name, name = parse_name(key)
+            result, measurements = parse_metric(value)
+            data.append({
+                "name": name,
+                "group_name": group_name,
+                "result": result,
+                "measurements": measurements,
+            })
+
         return data
---
+++ test/core/test_metrics_data.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+
+from squad.core.data import JSONMetricDataParser
+
+TEST_DATA = """
+{
+    "ungrouped_int": 10,
+    "ungrouped_float": 20.5,
+    "ungrouped_multiple": [10,11,10,11],
+    "group1/var": 50,
+    "group2/var": 60
+}
+"""
+parser = JSONMetricDataParser()
+
+
+class JSONMetricDataParserTest(TestCase):
+
+    def test_basics(self):
+        data = parser(TEST_DATA)
+        self.assertEqual(5, len(data))
+        self.assertIsInstance(data, list)
+
+    def test_metric_name(self):
+        data = parser(TEST_DATA)
+        names = [t['name'] for t in data]
+        self.assertIn('ungrouped_int', names)
+
+    def test_grouping(self):
+        data = parser(TEST_DATA)
+        item = [t for t in data if t['group_name'] == 'group1'][0]
+        self.assertEqual(item['name'], 'var')
+
+    def test_int(self):
+        data = parser(TEST_DATA)
+        item = [t for t in data if t['name'] == 'ungrouped_int'][0]
+        self.assertEqual(10, item['result'])
+        self.assertEqual([10], item['measurements'])
+
+    def test_float(self):
+        data = parser(TEST_DATA)
+        item = [t for t in data if t['name'] == 'ungrouped_float'][0]
+        self.assertEqual(20.5, item['result'])
+
+    def test_array(self):
+        data = parser(TEST_DATA)
+        item = [t for t in data if t['name'] == 'ungrouped_multiple'][0]
+        self.assertEqual(10.5, item['result'])
+        self.assertEqual([10, 11, 10, 11], item['measurements'])
+
+    def test_nested_group(self):
+        item = parser('{"foo/bar/baz": 1}')[0]
+        self.assertEqual('foo/bar', item['group_name'])
+        self.assertEqual('baz', item['name'])
