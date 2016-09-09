d6c81c19ce3b76b66137cd8c6e29855e4df8488c
Antonio Terceiro <antonio.terceiro@linaro.org>
2016-09-09 18:31:33 -0300

    test_code_quality: make flake8 test fail instead of error

--- test/test_code_quality.py
+++ test/test_code_quality.py
@@ -7,6 +7,6 @@ from django.test import TestCase
 if shutil.which('flake8'):
     class TestCodeQuality(TestCase):
         def test_flake8(self):
-            subprocess.check_call(['flake8'])
+            self.assertEqual('', subprocess.getoutput('flake8'))
 else:
     print("I: skipping flake8 test (flake8 not available)")
