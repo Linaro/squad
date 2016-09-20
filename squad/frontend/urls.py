e3417cf3c6d186c252db570c5746a1f34d1215c8
Antonio Terceiro <antonio.terceiro@linaro.org>
2016-09-20 18:31:19 -0300

    frontend: fix urls to match correct views
    They were missing a trailing $

--- squad/frontend/urls.py
+++ squad/frontend/urls.py
@@ -5,6 +5,6 @@ from . import views
 slug_pattern = '[a-z0-9_.-]+'
 urlpatterns = [
     url(r'^$', views.home),
-    url(r'^(%s)/' % slug_pattern, views.group),
-    url(r'^(%s)/(%s)/' % ((slug_pattern,) * 2), views.project),
+    url(r'^(%s)/$' % slug_pattern, views.group),
+    url(r'^(%s)/(%s)/$' % ((slug_pattern,) * 2), views.project),
 ]
