70c9130dba1e51e1843ce79298fcec69b50380a4
Antonio Terceiro <antonio.terceiro@linaro.org>
2016-09-22 12:43:21 -0300

    frontend: give name to urls

--- squad/frontend/urls.py
+++ squad/frontend/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 slug_pattern = '[a-z0-9_.-]+'
 urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^(%s)/$' % slug_pattern, views.group),
-    url(r'^(%s)/(%s)/$' % ((slug_pattern,) * 2), views.project),
+    url(r'^$', views.home, name='home'),
+    url(r'^(%s)/$' % slug_pattern, views.group, name='group'),
+    url(r'^(%s)/(%s)/$' % ((slug_pattern,) * 2), views.project, name='project'),
 ]
