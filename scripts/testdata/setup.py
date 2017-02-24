3a9a86984d5da35949f969655ea01a65ef8ed872
Antonio Terceiro <antonio.terceiro@linaro.org>
2017-02-24 15:00:59 -0300

    setup-dev: create multiple environments per build

--- scripts/testdata/setup.py
+++ scripts/testdata/setup.py
@@ -2,9 +2,12 @@ from squad.core.models import Group, Project
 
 team1 = Group.objects.create(slug='team1')
 team2 = Group.objects.create(slug='team2')
+team3 = Group.objects.create(slug='team3')
 
 project1 = team1.projects.create(slug='project1')
 project2 = team2.projects.create(slug='project2')
+project3 = team3.projects.create(slug='project3')
 
 token1 = project1.tokens.create(key='key1', description='Key 1')
 token2 = project2.tokens.create(key='key2', description='Key 2')
+token3 = project3.tokens.create(key='key3', description='Key 3')
