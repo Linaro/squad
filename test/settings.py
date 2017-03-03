2aff14605a2dbd4b39d438ef259c49322f4c0a79
Antonio Terceiro <antonio.terceiro@linaro.org>
2017-03-03 16:08:25 -0300

    test.settings: remove whitenoise middleware

--- test/settings.py
+++ test/settings.py
@@ -2,4 +2,6 @@ from squad.settings import *  # noqa
 
 LOGGING = {}
 
+# see  https://github.com/evansd/whitenoise/issues/94
+MIDDLEWARE_CLASSES.remove('whitenoise.middleware.WhiteNoiseMiddleware')  # noqa
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
