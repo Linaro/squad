8282491cd48d3a9341d88a0b126912a662b9b0a9
Antonio Terceiro <antonio.terceiro@linaro.org>
2017-03-31 15:38:24 -0300

    test.settings: actually disable logging

--- test/settings.py
+++ test/settings.py
@@ -1,6 +1,7 @@
 from squad.settings import *  # noqa
 
-LOGGING = {}
+LOGGING['loggers']['django']['level'] = 999  # noqa
+LOGGING['loggers']['']['level'] = 999  # noqa
 
 # see  https://github.com/evansd/whitenoise/issues/94
 MIDDLEWARE_CLASSES.remove('whitenoise.middleware.WhiteNoiseMiddleware')  # noqa
