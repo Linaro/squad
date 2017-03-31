d6a6ebfdb5e6bc6eaf90a9d7baabee0c3694edcc
Antonio Terceiro <antonio.terceiro@linaro.org>
2017-03-31 09:06:37 -0300

    ci: basic admin integration

--- squad/ci/admin.py
+++ squad/ci/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from squad.ci.models import Backend, TestJob
 
-# Register your models here.
+
+class BackendAdmin(admin.ModelAdmin):
+    list_display = ('url', 'implementation_type')
+
+
+class TestJobAdmin(admin.ModelAdmin):
+    list_filter = ('submitted', 'fetched')
+
+
+admin.site.register(Backend, BackendAdmin)
+admin.site.register(TestJob, TestJobAdmin)
