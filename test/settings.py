91622d839eaf7f2d0ee8e6bf61b1c7f195464efc
Antonio Terceiro <antonio.terceiro@linaro.org>
2017-04-04 16:10:12 -0300

    test.settings: revert most of the speedup changes
    At least one of them is causing strange errors on CI, and as it turns
    out, most of the speed gains come from just disabling logging.

--- test/settings.py
+++ test/settings.py
@@ -1,15 +1,7 @@
 from squad.settings import *  # noqa
 import logging
 
-# settings to speed up tests
-# source: http://www.daveoncode.com/2013/09/23/effective-tdd-tricks-to-speed-up-django-tests-up-to-10x-faster/
-PASSWORD_HASHERS = (
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-)
-DEBUG = False
-TEMPLATE_DEBUG = False
 logging.disable(logging.CRITICAL)
-
 
 # see  https://github.com/evansd/whitenoise/issues/94
 MIDDLEWARE_CLASSES.remove('whitenoise.middleware.WhiteNoiseMiddleware')  # noqa
