0305b8a8d37ae276906a2ed3da6ec035c11385ee
Antonio Terceiro <antonio.terceiro@linaro.org>
2017-04-04 15:57:33 -0300

    test.settings: speed up tests

--- test/settings.py
+++ test/settings.py
@@ -1,7 +1,15 @@
 from squad.settings import *  # noqa
+import logging
 
-LOGGING['loggers']['django']['level'] = 999  # noqa
-LOGGING['loggers']['']['level'] = 999  # noqa
+# settings to speed up tests
+# source: http://www.daveoncode.com/2013/09/23/effective-tdd-tricks-to-speed-up-django-tests-up-to-10x-faster/
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
+DEBUG = False
+TEMPLATE_DEBUG = False
+logging.disable(logging.CRITICAL)
+
 
 # see  https://github.com/evansd/whitenoise/issues/94
 MIDDLEWARE_CLASSES.remove('whitenoise.middleware.WhiteNoiseMiddleware')  # noqa
