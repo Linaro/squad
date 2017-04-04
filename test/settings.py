9bb6360b478ec895c155252c9057ac429d901f8b
Antonio Terceiro <antonio.terceiro@linaro.org>
2017-04-04 16:38:56 -0300

    test.settings: revert log disabling as well

--- test/settings.py
+++ test/settings.py
@@ -1,7 +1,9 @@
 from squad.settings import *  # noqa
 import logging
 
-logging.disable(logging.CRITICAL)
+
+LOGGING['loggers']['django']['level'] = 999  # noqa
+LOGGING['loggers']['']['level'] = 999  # noqa
 
 # see  https://github.com/evansd/whitenoise/issues/94
 MIDDLEWARE_CLASSES.remove('whitenoise.middleware.WhiteNoiseMiddleware')  # noqa
