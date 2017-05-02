8c66a7d44f4ba263b556ee87e3215ca78c497b09
Antonio Terceiro <antonio.terceiro@linaro.org>
2017-05-02 11:41:09 -0300

    frontent: redirect /api to /
    In the future we might change that to providing some (potentially
    auto-generated) documentation at /api. For now, just preventing /api to
    be interpreted as a visit to an group called "api" is good enough.

--- squad/api/urls.py
+++ squad/api/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.shortcuts import redirect
 
 
 from . import views
@@ -10,6 +11,7 @@ slug_pattern = '[a-z0-9_.-]+'
 
 
 urlpatterns = [
+    url(r'^$', lambda request: redirect('/')),
     url(r'^submit/(%s)/(%s)/(%s)/(%s)' % ((slug_pattern,) * 4), views.add_test_run),
     url(r'^submitjob/(%s)/(%s)/(%s)/(%s)' % ((slug_pattern,) * 4), ci.submit_job),
     url(r'^data/(%s)/(%s)' % ((slug_pattern,) * 2), data.get),
