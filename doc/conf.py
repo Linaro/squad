4fed35f565589160ce8960495bfaff640c5e26c7
Antonio Terceiro <antonio.terceiro@linaro.org>
2017-10-11 17:56:25 -0300

    doc: fix copyright

--- doc/conf.py
+++ doc/conf.py
@@ -50,8 +50,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'SQUAD'
-copyright = '2017, SQUAD developer team'
-author = 'SQUAD developer team'
+copyright = '2016-2017, Linaro Limited'
+author = 'Linaro'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -146,7 +146,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'SQUAD.tex', 'SQUAD Documentation',
-     'SQUAD developer team', 'manual'),
+     'Linaro', 'manual'),
 ]
 
 
