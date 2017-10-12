ed78fcee082cce2bcfcdd3deee8c2b1370a7362d
Antonio Terceiro <antonio.terceiro@linaro.org>
2017-10-12 10:09:18 -0300

    plugins: add basic documentation

--- squad/plugins/__init__.py
+++ squad/plugins/__init__.py
@@ -39,6 +39,24 @@ def get_plugin_instance(name):
 
 
 def apply_plugins(plugin_names):
+    """
+    This function should be used by code in the SQUAD core to trigger
+    functionality from plugins.
+
+    The ``plugin_names`` argument is list of plugins names to be used. Most
+    probably, you will want to pass the list of plugins enabled for a given
+    project, e.g.  ``project.enabled_plugins``.
+
+    Example::
+
+        from squad.plugins import apply_plugins
+
+        # ...
+
+        for plugin in apply_plugins(project.enabled_plugins):
+            plugin.method(...)
+
+    """
     for p in plugin_names:
         try:
             plugin = get_plugin_instance(p)
@@ -48,6 +66,23 @@ def apply_plugins(plugin_names):
 
 
 class Plugin(object):
+    """
+    This class must be used as a superclass for all SQUAD plugins. All the
+    methods declared here have empty implementations (i.e. they do nothing),
+    and should be overriden in your plugin to provide extra functionality to
+    the SQUAD core.
+    """
 
     def postprocess_testrun(self, testrun):
+        """
+        This method is called after a test run has been received by SQUAD, and
+        the test run data (tests, metrics, metadata, logs, etc) have been saved
+        to the database.
+
+        You can use this method to parse logs, do any special handling of
+        metadata, test results, etc.
+
+        The ``testrun`` arguments is an instance of
+        ``squad.core.models.TestRun``.
+        """
         pass
