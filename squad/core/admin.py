243a7cecbaa7c68eb78b56fbf3fb46d31d501828
Antonio Terceiro <antonio.terceiro@linaro.org>
2017-12-14 16:10:00 -0200

    core/admin: add SuiteMetadata

--- squad/core/admin.py
+++ squad/core/admin.py
@@ -103,9 +103,18 @@ class BuildAdmin(admin.ModelAdmin):
     list_filter = ['project', 'datetime']
 
 
+class SuiteMetadataAdmin(admin.ModelAdmin):
+    models = models.SuiteMetadata
+    ordering = ['name']
+    list_display = ['__str__', 'kind', 'description']
+    list_filter = ['kind', 'suite']
+    readonly_fields = ['kind', 'suite', 'name']
+
+
 admin.site.register(models.Group)
 admin.site.register(models.Project, ProjectAdmin)
 admin.site.register(models.EmailTemplate)
 admin.site.register(models.Token, TokenAdmin)
 admin.site.register(models.ProjectStatus, ProjectStatusAdmin)
 admin.site.register(models.Build, BuildAdmin)
+admin.site.register(models.SuiteMetadata, SuiteMetadataAdmin)
