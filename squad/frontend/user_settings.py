17ed73409168428c9f8b75d76527058606a83550
Antonio Terceiro <antonio.terceiro@linaro.org>
2018-02-02 14:42:52 -0200

    frontend: add API token settings page

--- squad/frontend/user_settings.py
+++ squad/frontend/user_settings.py
@@ -7,6 +7,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 
+from rest_framework.authtoken.models import Token
+
+
 from squad.http import auth
 
 
@@ -35,7 +38,25 @@ def profile(request):
     return render(request, 'squad/user_settings/profile.html', context)
 
 
+@login_required
+def api_token(request):
+    try:
+        token = Token.objects.get(user=request.user)
+    except Token.DoesNotExist:
+        token = None
+
+    if request.method == "POST":
+        if token:
+            token.delete()
+        token = Token.objects.create(user=request.user)
+        return redirect(reverse('settings-api-token'))
+
+    context = {'token': token}
+    return render(request, 'squad/user_settings/api_token.html', context)
+
+
 urls = [
     url('^$', home, name='settings-home'),
     url('^profile/$', profile, name='settings-profile'),
+    url('^api-token/$', api_token, name='settings-api-token'),
 ]
