4c8e22d6a59f1bcb1ace9e6b6d3a0f6fa5da7397
Antonio Terceiro <antonio.terceiro@linaro.org>
2018-02-16 14:00:24 -0200

    Add CORS support via django-cors-headers
    This should allow the API to be consumed via Javascript from any site.
    For now, only read-only operations (GET, HEAD) are allowed, which should
    be fine for most use cases.

---
+++ test/test_cors.py
@@ -0,0 +1,15 @@
+import json
+from django.test import Client, TestCase
+
+
+class CrossOriginResourceSharingTest(TestCase):
+
+    def setUp(self):
+        client = Client()
+        self.response = client.options('/', {}, **{'HTTP_ORIGIN': 'https://www.example.com/'})
+
+    def test_allow_origin(self):
+        self.assertEqual('*', self.response['Access-Control-Allow-Origin'])
+
+    def test_allow_methods(self):
+        self.assertEqual('GET, HEAD', self.response['Access-Control-Allow-Methods'])
