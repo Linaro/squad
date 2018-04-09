90d49c74ad366f741d9c09eceb8fff57e27452a2
Antonio Terceiro <antonio.terceiro@linaro.org>
2018-04-09 16:19:06 -0300

    scripts/git-build: don't touch squad/__init__.py

--- squad/__init__.py
+++ squad/__init__.py
@@ -1,5 +1,10 @@
 from __future__ import absolute_import
+import os
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app  # noqa
+try:
+    from .celery import app as celery_app  # noqa
+except ModuleNotFoundError as e:
+    if not os.environ.get('SQUAD_GIT_BUILD'):
+        raise e
