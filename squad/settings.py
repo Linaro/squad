cfd6a662ac76c8c5a4f21677e66f8e99e082ea29
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2018-08-23 13:49:54 +0100

    api: disable web API forms
    Web API forms make the page rendering slow and they should not be used
    by ordinary users.
    
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- squad/settings.py
+++ squad/settings.py
@@ -272,6 +272,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'squad.api.renderers.BrowsableAPIRendererWithoutForms',
     )
 }
 
