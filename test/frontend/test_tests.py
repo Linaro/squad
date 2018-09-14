5b310163d8920079b15a66f642503b8fcf5ea0d0
Antonio Terceiro <antonio.terceiro@linaro.org>
2018-09-14 15:25:03 -0300

    frontend: fix "All test results" with xfail results

--- test/frontend/test_tests.py
+++ test/frontend/test_tests.py
@@ -1,6 +1,5 @@
-from squad.core import models as core_models
+from squad.core import models
 from squad.core.tasks import ReceiveTestRun
-from squad.ci import models
 from django.test import TestCase
 from django.test import Client
 import json
@@ -10,7 +9,10 @@ tests_file = {
     ("suite1/test%d" % i): "fail" for i in range(0, 50)
 }
 tests_file.update({
-    ("suite2/test%d" % i): "pass" for i in range(0, 50)
+    ("suite2/test%d" % i): "fail" for i in range(0, 50)  # xfail!
+})
+tests_file.update({
+    ("suite3/test%d" % i): "pass" for i in range(0, 50)
 })
 
 
@@ -18,8 +20,18 @@ class AllTestResultsTest(TestCase):
 
     def setUp(self):
         self.client = Client()
-        group = core_models.Group.objects.create(slug='mygroup')
+        group = models.Group.objects.create(slug='mygroup')
         project = group.projects.create(slug='myproject')
+        env = project.environments.create(slug='myenv')
+
+        for test, _ in tests_file.items():
+            if test.startswith('suite2/'):
+                issue = models.KnownIssue.objects.create(
+                    title='foo fails',
+                    test_name=test
+                )
+                issue.environments.add(env)
+
         ReceiveTestRun(project)(
             version='1',
             environment_slug='myenv',
@@ -34,15 +46,26 @@ class AllTestResultsTest(TestCase):
         response = self.client.get('/mygroup/myproject/build/1/tests/')
         self.assertEqual(200, response.status_code)
 
-    def test_pagination(self):
-        # page 1: only tests from suite1, which fail
+    def test_pagination_page_1(self):
+        # page 1: only tests from suite1 -  fail
         response = self.client.get('/mygroup/myproject/build/1/tests/?page=1')
         page1 = str(response.content)
         self.assertTrue("suite1" in page1)
         self.assertTrue("suite2" not in page1)
+        self.assertTrue("suite3" not in page1)
 
-        # page 2: only tests from suite2, which pass
+    def test_pagination_page_2(self):
+        # page 2: only tests from suite2 - xfail
         response = self.client.get('/mygroup/myproject/build/1/tests/?page=2')
         page2 = str(response.content)
         self.assertTrue("suite1" not in page2)
         self.assertTrue("suite2" in page2)
+        self.assertTrue("suite3" not in page2)
+
+    def test_pagination_page_3(self):
+        # page 3: only tests from suite3 - pass
+        response = self.client.get('/mygroup/myproject/build/1/tests/?page=3')
+        page3 = str(response.content)
+        self.assertTrue("suite1" not in page3)
+        self.assertTrue("suite2" not in page3)
+        self.assertTrue("suite3" in page3)
