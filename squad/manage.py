f04b802b95adf2fd2d56242c93d8efb027ffe0de
Antonio Terceiro <antonio.terceiro@linaro.org>
2018-10-08 17:15:09 -0300

    squad.manage: check performance counts when running tests

--- squad/manage.py
+++ squad/manage.py
@@ -3,12 +3,19 @@ import sys
 
 
 def main():
+    testing = False
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test.settings")
+        testing = True
     else:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "squad.settings")
     from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
+    try:
+        execute_from_command_line(sys.argv)
+    finally:
+        if testing:
+            import test.performance
+            test.performance.export('tmp/queries.json')
 
 
 if __name__ == "__main__":
