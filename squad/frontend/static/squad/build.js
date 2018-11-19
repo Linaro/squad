ed2e89a3b562dc6b251a76d093ef815bfd756ffc
Charles <18oliveira.charles@gmail.com>
2018-11-19 11:25:50 -0200

    fix testjobs resubmit
    There were two ng-app in testjobs.jinja2, moved ResubmitController
    to build.js. We should make one app per page and then share controllers
    among ng-apps

--- squad/frontend/static/squad/build.js
+++ squad/frontend/static/squad/build.js
@@ -84,6 +84,37 @@ function FilterController($scope, $attrs, $location) {
   $scope.init()
 }
 
+function ResubmitController($scope, $http, $location, $timeout) {
+
+    $scope.error = false
+    $scope.loading = false
+    $scope.done = false
+    $scope.resubmit = function(test_job_id, force) {
+        if ($scope.done) return
+        $scope.loading = true
+
+        var endpoint = force ? "/api/forceresubmit/" : "/api/resubmit/";
+
+        $http.get(endpoint + test_job_id).then(
+            function(response) {
+                $timeout(function() {
+                    $scope.loading = false
+                    $scope.done = true
+                }, 1000)
+            },
+            function(response) {
+                var msg = "There was an error while resubmitting.\n" +
+                    "Status = " + response.status + " " + response.statusText +
+                    "(" + response.xhrStatus + ")"
+                alert(msg)
+                $scope.loading = false
+                $scope.error = true
+                $scope.done = true
+            }
+        )
+    }
+}
+
 app.controller(
     'AnnotationController',
     [
@@ -103,3 +134,14 @@ app.controller(
         FilterController
     ]
 )
+
+app.controller(
+    'ResubmitController',
+    [
+        '$scope',
+        '$http',
+        '$location',
+        '$timeout',
+        ResubmitController
+    ]
+);
