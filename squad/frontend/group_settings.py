dd6ad426c2803f7c11eb888d5fb249e9e830913d
Antonio Terceiro <antonio.terceiro@linaro.org>
2019-03-01 11:30:28 -0300

    frontend: add "New project" to group UI

--- squad/frontend/group_settings.py
+++ squad/frontend/group_settings.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.conf.urls import url
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 from squad.core.models import Group
+from squad.core.models import Project
 from squad.http import auth_write
 
 
@@ -30,6 +31,34 @@ def settings(request, group_slug):
     return render(request, 'squad/group_settings/index.jinja2', context)
 
 
+class NewProjectForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['group'].widget = forms.HiddenInput()
+        self.fields['group'].disabled = True
+
+    class Meta:
+        model = Project
+        fields = ['group', 'slug', 'name', 'is_public', 'description']
+
+
+@auth_write
+def new_project(request, group_slug):
+    group = request.group
+    if request.method == "POST":
+        form = NewProjectForm(request.POST, instance=Project(group=group))
+        if form.is_valid():
+            project = form.save()
+            return redirect(reverse('project-settings', args=[group.slug, project.slug]))
+    else:
+        form = NewProjectForm(instance=Project(group=group))
+
+    context = {'group': group, 'form': form}
+    return render(request, 'squad/group_settings/new_project.jinja2', context)
+
+
 urls = [
     url('^$', settings, name='group-settings'),
+    url('^new-project/$', new_project, name='group-new-project'),
 ]
