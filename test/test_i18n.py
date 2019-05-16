83ba6b1bcc5eda1c77cd7be9979157c8d2dc4813
Antonio Terceiro <antonio.terceiro@linaro.org>
2019-05-16 15:35:31 -0300

    i18n: rename pl_PL locale to pl
    The list of locales supported by Django includes 'pl', but not 'pl-pl'.

---
+++ test/test_i18n.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.conf import settings
+from django.conf import global_settings
+from glob import glob
+from os.path import dirname, basename
+
+
+class TestI18N(TestCase):
+
+    def test_locales_supported_by_django(self):
+        django_languages = set(l for l, _ in global_settings.LANGUAGES)
+        languages = glob('%s/squad/*/locale/*/LC_MESSAGES' % settings.BASE_DIR)
+        languages = set(basename(dirname(l.lower().replace('_', '-'))) for l in languages)
+        intersection = django_languages & languages
+        self.assertEqual(intersection, languages, 'language code not supported by Django. Must be one of %r' % sorted(django_languages))
