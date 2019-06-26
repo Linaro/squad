4a184a2127c331437732d7901d4bbbf1b5ccce56
Charles <18oliveira.charles@gmail.com>
2019-06-26 15:51:23 -0300

    frontend: order projects alphanumerically

--- test/frontend/test_utils.py
+++ test/frontend/test_utils.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from squad.frontend.utils import file_type
+from squad.core.models import Group, Project
+from squad.frontend.utils import file_type, alphanum_sort
 
 
 class FileTypeTest(TestCase):
@@ -15,3 +16,24 @@ class FileTypeTest(TestCase):
     def test_image(self):
         self.assertEqual('image', file_type('foo.png'))
         self.assertEqual('image', file_type('foo.jpg'))
+
+
+class AlphaNumSortTest(TestCase):
+
+    def setUp(self):
+        self.group = Group.objects.create(slug='mygroup')
+        self.p1 = self.group.projects.create(slug='project1', name='project-v1.1')
+        self.p2 = self.group.projects.create(slug='project2', name='project-v1.10')
+        self.p3 = self.group.projects.create(slug='project3', name='project-v1.2')
+
+    def test_asc_sort(self):
+        projects = Project.objects.all()
+        projects_sorted = alphanum_sort(projects, 'name', reverse=False)
+        # v1.1 -> v1.2 -> v1.10
+        self.assertEqual([self.p1, self.p3, self.p2], projects_sorted)
+
+    def test_desc_sort(self):
+        projects = Project.objects.all()
+        projects_sorted = alphanum_sort(projects, 'name')
+        # v1.10 -> v1.2 -> v1.1
+        self.assertEqual([self.p2, self.p3, self.p1], projects_sorted)
