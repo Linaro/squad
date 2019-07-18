a65c996c36e118619afc7fa1ec0f87d768f60662
Charles <18oliveira.charles@gmail.com>
2019-07-18 10:16:24 -0300

    frontend: compare-tests: fix auto complete initialization
    Fix auto complete when a user first clicks on a dropdown.
    
    Before, a user would have to start typing to see some results
    for project/suite/tests. This patch fixes the issue by making
    an initial query on click.

--- squad/frontend/static/squad/controllers/compare.js
+++ squad/frontend/static/squad/controllers/compare.js
@@ -171,14 +171,11 @@ function CompareController($scope, $http, $location) {
             url: "/api/projects",
             dataType: 'json',
             data: function (params) {
-                var filters = "%28group__slug__icontains%253D" +
-                    params.term +
-                    "%29%20%7C%20%28group__name__icontains%253D" +
-                    params.term +
-                    "%29%20%7C%20%28name__icontains%253D" +
-                    params.term +
-                    "%29%20%7C%20%28slug__icontains%253D" +
-                    params.term +
+                var term = params.term == undefined ? '' : params.term
+                var filters = "%28group__slug__icontains%253D" + term +
+                    "%29%20%7C%20%28group__name__icontains%253D" + term +
+                    "%29%20%7C%20%28name__icontains%253D" + term +
+                    "%29%20%7C%20%28slug__icontains%253D" + term +
                     "%29";
                 return {
                     filters: filters, // search term
@@ -216,8 +213,9 @@ function CompareController($scope, $http, $location) {
             url: "/api/suitemetadata/",
             dataType: 'json',
             data: function (params) {
+                var term = params.term == undefined ? '' : params.term
                 return {
-                    suite__startswith: params.term, // search term
+                    suite__startswith: term, // search term
                     project: $scope.getProjectIds().join(),
                     kind: 'suite',
                     offset: params.page * 50 || 0,
@@ -254,8 +252,9 @@ function CompareController($scope, $http, $location) {
             url: "/api/suitemetadata/",
             dataType: 'json',
             data: function (params) {
+                var term = params.term == undefined ? '' : params.term
                 return {
-                    name__startswith: params.term, // search term
+                    name__startswith: term, // search term
                     project: $scope.getProjectIds().join(),
                     kind: 'test',
                     suite: $scope.selectedSuite,
