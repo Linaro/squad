7f9dda1d538c44e5c04061743f1d6bda172f0042
Antonio Terceiro <antonio.terceiro@linaro.org>
2019-10-08 12:06:27 -0300

    squad.run.worker: listen on all configured queues by default
    This way one does not need to explicitly pass the queue names in the
    command line

--- squad/run/worker.py
+++ squad/run/worker.py
@@ -1,15 +1,17 @@
+from squad.settings import CELERY_TASK_ROUTES
 import os
 import sys
 
 
 def main():
+    queues = set([conf['queue'] for _, conf in CELERY_TASK_ROUTES.items()])
     argv = [
         sys.executable, '-m', 'celery',
         # default celery args:
         '-A', 'squad',
         'worker',
+        '--queues=celery,' + ','.join(queues),
         '--concurrency=1',
-        '--queues=celery,reporting_queue',
         '--max-tasks-per-child=5000',
         '--max-memory-per-child=1500000',
         '--loglevel=INFO'
