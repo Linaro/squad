f3ba6a8ae786f42f34fbdb020601de5a20d7f05d
Charles Oliveira <charles.oliveira@linaro.org>
2019-10-18 21:19:11 -0300

    api: disable post forms
    DRF comes with built-in forms that help users to make queries
    in the API. But that means select boxes to fill up, and that takes
    up lots of trips to database.
    
    This patch disable such forms completely, making overall API calls a bit faster
    when not using json format.

--- squad/settings.py
+++ squad/settings.py
@@ -321,7 +321,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'squad.api.utils.BrowsableAPIRendererWithoutForms',
+    ),
 }
 
 # CORS setup
