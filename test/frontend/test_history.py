4722f76e6ca6d2c90e50d0feb798c24236d79bd9
Charles Oliveira <charles.oliveira@linaro.org>
2020-04-13 10:18:13 -0300

    core: history: check for empty suite metadata

--- test/frontend/test_history.py
+++ test/frontend/test_history.py
@@ -21,3 +21,20 @@ class TestHistoryWithNoData(TestCase):
     def test_history_with_unexisting_suite_name(self):
         response = self.client.get('/mygroup/myproject/tests/foo/bar')
         self.assertEqual(404, response.status_code)
+
+
+class TestHistoryTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        group = Group.objects.create(slug='mygroup')
+        project = group.projects.create(slug='myproject')
+        env = project.environments.create(slug='myenv')
+        suite = project.suites.create(slug='mysuite')
+        build = project.builds.create(version='mybuild')
+        testrun = build.test_runs.create(job_id='123', environment=env)
+        testrun.tests.create(name='mytest', suite=suite)
+
+    def test_tests_history_with_empty_suite_metadata(self):
+        response = self.client.get('/mygroup/myproject/tests/mysuite/mytest')
+        self.assertEqual(200, response.status_code)
