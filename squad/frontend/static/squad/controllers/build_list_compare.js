24c415393d2b6f125393084fc7249b32d52a2511
Amro Hassaan <amro.hassaan@linaro.org>
2020-07-27 14:41:31 -0400

    frontend: add choice of comparison type
    enable user to choose between metrics and tests when comparing builds in
    project view and builds list view.

--- squad/frontend/static/squad/controllers/build_list_compare.js
+++ squad/frontend/static/squad/controllers/build_list_compare.js
@@ -1,9 +1,11 @@
 export function BuildListCompareController($scope, $window) {
     $scope.baseline_build = ''
     $scope.target_build = ''
+    $scope.comparison_type = ''
     $scope.compareBuilds = function(project) {
         var baseline = $scope.baseline_build
         var target = $scope.target_build
+        var comparison_type = $scope.comparison_type
         if(baseline == '' || target == '') {
             alert($scope.invalid_number_of_builds_msg)
         }
@@ -14,6 +16,7 @@ export function BuildListCompareController($scope, $window) {
             var path = '/_/comparebuilds/?project=' + project
             path += '&baseline=' + baseline
             path += '&target=' + target
+            path += '&comparison_type=' + comparison_type
             $window.location.href = path
         }
     }
