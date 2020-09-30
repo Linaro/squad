29e6a0a0f3ac67058bb4d0ec9a9834f8642bf6e2
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2020-09-30 14:23:59 +0100

    core: make PatchSource.token nullable
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- squad/core/models.py
+++ squad/core/models.py
@@ -336,7 +336,7 @@ class PatchSource(models.Model):
     username = models.CharField(max_length=128)
     _password = models.CharField(max_length=128, null=True, blank=True, db_column="password")
     url = CustomURLField(help_text="scheme://host, ex: 'http://github.com', 'ssh://gerrit.host, etc'")
-    token = models.CharField(max_length=1024, blank=True)
+    token = models.CharField(max_length=1024, blank=True, null=True)
     implementation = PluginField(
         default='null',
         features=[
