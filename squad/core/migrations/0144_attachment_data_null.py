af1110c5c96e014565c4fca2f6e24a58adc78319
Charles Oliveira <charles.oliveira@linaro.org>
2020-10-27 07:30:02 -0300

    core: migrations: fix storage migrations

--- squad/core/migrations/0144_attachment_data_null.py
+++ squad/core/migrations/0144_attachment_data_null.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0139_attachment_storage'),
+        ('core', '0143_attachment_storage'),
     ]
 
     operations = [
