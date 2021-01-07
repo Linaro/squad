c18d4119e05618b5b2fffe57426dd8741b64b2dd
Charles Oliveira <charles.oliveira@linaro.org>
2021-01-07 11:41:55 -0300

    test: add test to check for pending migrations
    This will fail tests if there are migrations that need
    to be created

---
+++ test/test_pending_migrations.py
@@ -0,0 +1,19 @@
+from io import StringIO
+from unittest import TestCase
+
+
+from django.core import management
+
+
+class TestPendingMigrations(TestCase):
+    def test_pending_migrations(self):
+        output = StringIO()
+        management.call_command('makemigrations', '--dry-run', stdout=output)
+
+        expected_output = 'No changes detected\n'
+        pending_migrations = output.getvalue()
+        if expected_output != pending_migrations:
+            print('There are pending migrations:')
+            print(pending_migrations)
+
+        self.assertEqual(expected_output, pending_migrations)
