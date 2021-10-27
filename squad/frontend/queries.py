01998b9e4668e71602edcde451e851575a01ee80
Charles Oliveira <charles.oliveira@linaro.org>
2021-10-27 08:32:55 -0300

    frontend: queries: fetch metrics faster
    This avoids a join between environment and metrics via test_run
    by using the environment straight from metrics.
    
    Signed-off-by: Charles Oliveira <charles.oliveira@linaro.org>

--- squad/frontend/queries.py
+++ squad/frontend/queries.py
@@ -6,12 +6,9 @@ from squad.core.utils import join_name, split_list
 def get_metrics_list(project):
     unique_names = set()
 
-    testruns = TestRun.objects.filter(environment__project=project).values('id').order_by('id')
-    test_runs_ids = [tr['id'] for tr in testruns]
-    for chunk in split_list(test_runs_ids, chunk_size=100):
-        metric_set = Metric.objects.filter(test_run_id__in=chunk).values('suite__slug', 'name')
-        for m in metric_set:
-            unique_names.add(join_name(m['suite__slug'], m['name']))
+    metric_set = Metric.objects.filter(environment__project=project).values('suite__slug', 'name')
+    for m in metric_set:
+        unique_names.add(join_name(m['suite__slug'], m['name']))
 
     metric_names = [{"name": name} for name in sorted(unique_names)]
 
