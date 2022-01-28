e791928c6f23c0f0420f79bd0669a6a1d28f1e83
Charles Oliveira <charles.oliveira@linaro.org>
2022-01-28 11:16:40 -0300

    frontend: ci: remove unecessary queries
    I don't know why this page was requesting testruns to be prefetched.
    So I removed them one by one while checking on debug toolbar if
    they were really needed or not. This sped up the page by a lot!
    
    Signed-off-by: Charles Oliveira <charles.oliveira@linaro.org>

--- squad/frontend/ci.py
+++ squad/frontend/ci.py
@@ -2,12 +2,10 @@ import django_filters
 from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render
 from django.http import Http404
-from django.db.models import Prefetch
 from django.utils.translation import ugettext_lazy as N_
 
 from squad.http import auth
 from squad.ci.models import TestJob
-from squad.core.models import TestRun
 
 
 class TestjobFilter(django_filters.FilterSet):
@@ -26,21 +24,15 @@ class TestjobFilter(django_filters.FilterSet):
 
 @auth
 def testjobs(request, group_slug, project_slug, build_version):
-    testjobs = TestJob.objects.prefetch_related(
-        Prefetch('testrun', queryset=TestRun.objects.only('build', 'environment', 'job_id')),
-        'testrun__build__project__group',
-        'target__group',
-        'target_build',
-        'backend',
-        'parent_job__backend')
-
     project = request.project
-    build = project.builds.filter(version=build_version).prefetch_related(Prefetch('test_jobs', queryset=testjobs), 'annotation').last()
+    build = project.builds.filter(version=build_version).prefetch_related('annotation').last()
     if build is None:
         raise Http404()
 
+    testjobs = TestJob.objects.filter(target_build=build).prefetch_related('backend', 'parent_job__backend').order_by('id')
+
     try:
-        testjob_filter = TestjobFilter(request.GET, queryset=build.test_jobs.all().order_by('id'))
+        testjob_filter = TestjobFilter(request.GET, queryset=testjobs)
         paginator = Paginator(testjob_filter.qs, 25)
         page = request.GET.get('page', 1)
         testjobs_page = paginator.page(page)
