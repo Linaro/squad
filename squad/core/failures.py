8ba9b02f8718a2dd74f33e15df392de07616e951
Justin Cook <justin.cook@linaro.org>
2022-04-20 16:15:58 -0500

    squad: core: failures: return failures with confidence data set
    Given a light weight queryset of failures, return the a full queryset of
    failures with their confidence data set.
    
    Signed-off-by: Justin Cook <justin.cook@linaro.org>

--- squad/core/failures.py
+++ squad/core/failures.py
@@ -67,3 +67,31 @@ class FailuresWithConfidence(object):
             f.set_confidence(self.project.build_confidence_threshold, f_p)
 
         return failures
+
+
+def failures_with_confidence(project, build, failures):
+        fwc = build.tests.filter(
+            result=False,
+            metadata_id__in=[f.metadata__id for f in failures],
+        ).prefetch_related(
+            'metadata', 'environment',
+        ).order_by(
+            'suite__slug', 'metadata__name'
+        )
+
+        limit = project.build_confidence_count
+        builds = project.builds.filter(
+            id__lt=build.id,
+        ).order_by("-id").only("id")[:limit]
+
+        previous = Test.objects.filter(
+            metadata__in=[f.metadata for f in fwc],
+            environment__in=[f.environment for f in fwc],
+            build_id__in=list(builds.values_list("id", flat=True)),
+        ).order_by("-build_id").prefetch_related("metadata", "environment").defer("log")
+
+        for f in fwc:
+            f_p = [t for t in previous if t.metadata == f.metadata and t.environment == f.environment]
+            f.set_confidence(project.build_confidence_threshold, f_p)
+
+        return fwc
